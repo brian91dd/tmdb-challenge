48e8dbd3f58833e78236993d469de1f5f41710ed
Brian <briandediego@gmail.com>
2021-02-02 17:19:56 -0300

    Add className to text input component

--- src/components/text-input/index.jsx
+++ src/components/text-input/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function TextInput({ handleChange, defaultValue, placeHolder }) {
+function TextInput({
+  handleChange, defaultValue, placeHolder, className,
+}) {
   return (
     <input
       onChange={handleChange}
       placeholder={placeHolder}
       defaultValue={defaultValue}
+      className={className}
     />
   );
 }
@@ -15,11 +18,13 @@ TextInput.propTypes = {
   handleChange: PropTypes.func.isRequired,
   defaultValue: PropTypes.string,
   placeHolder: PropTypes.string,
+  className: PropTypes.string,
 };
 
 TextInput.defaultProps = {
   defaultValue: '',
   placeHolder: '',
+  className: '',
 };
 
 export default TextInput;
